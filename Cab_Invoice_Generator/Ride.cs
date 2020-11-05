ce838bbe9dba129e13c3afebdf64155ea94905c2
Satantra Tewari <satantra1508@gmail.com>
2020-11-05 19:15:52 +0530

    [satantra] add. class added storing ride data like distance and time

---
+++ Cab_Invoice_Generator/Ride.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cab_Invoice_Generator
+{
+    public class Ride
+    {
+        public double distance;
+        public int time;
+        public Ride()
+        {
+            // default constructor
+            distance = 0.0;
+            time = 0;
+        }
+        public Ride(double distance, int time)
+        {
+            //parameterized constructor
+            this.distance = distance;
+            this.time = time;
+        }
+    }
+}
