8db990f5f09087a4b4de80e611b2a964bd019aff
Satantra Tewari <satantra1508@gmail.com>
2020-11-05 20:23:15 +0530

    [satantra] add. test cases run

--- Cab_Invoice_Generator_Program_Test/UnitTest1.cs
+++ Cab_Invoice_Generator_Program_Test/UnitTest1.cs
@@ -62,5 +62,29 @@ namespace Cab_Invoice_Generator_Program_Test
             Invoice_Data invoiceData = invoiceGenerator.GetSummary(listOfRides);
             Assert.IsTrue(invoiceData.numberOfRides == expectedRides && invoiceData.totalFare == expectedValue && invoiceData.averageFare == expectedAverage);
         }
+        [Test]
+        public void TestMethod7()
+        {
+            listOfRides = new List<Ride> { new Ride(5, 20), null, new Ride(2, 10) };
+            var exception = Assert.Throws<Cab_Invoice_Exception>(() => invoiceGenerator.AddRideDetails(1, listOfRides));
+            Assert.AreEqual(Cab_Invoice_Exception.ExceptionType.NULL_RIDES, exception.exceptionType);
+        }
+        [Test]
+        public void TestMethod8()
+        {
+            listOfRides = new List<Ride> { new Ride(5, 20), new Ride(3, 15), new Ride(2, 10) };
+            double expectedFare = 145;
+            int expectedRides = 3;
+            double expectedAverage = expectedFare / expectedRides;
+            invoiceGenerator.AddRideDetails(1, listOfRides);
+            Invoice_Data invoiceData = invoiceGenerator.GetInvoice(1);
+            Assert.IsTrue(invoiceData.numberOfRides == expectedRides && invoiceData.totalFare == expectedFare && invoiceData.averageFare == expectedAverage);
+        }
+        [Test]
+        public void TestMethod9()
+        {
+            var exception = Assert.Throws<Cab_Invoice_Exception>(() => invoiceGenerator.GetInvoice(1));
+            Assert.AreEqual(Cab_Invoice_Exception.ExceptionType.INVALID_USERID, exception.exceptionType);
+        }
     }
 }
