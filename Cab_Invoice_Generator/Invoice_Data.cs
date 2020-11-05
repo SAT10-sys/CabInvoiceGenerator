ec458c9cf136fa169a4d4d2549c35725e376509b
Satantra Tewari <satantra1508@gmail.com>
2020-11-05 19:57:03 +0530

    [satantra] add. enhanced data for summary

---
+++ Cab_Invoice_Generator/Invoice_Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cab_Invoice_Generator
+{
+    public class Invoice_Data
+    {
+        public int numberOfRides;
+        public double totalFare;
+        public double averageFare;
+        public Invoice_Data(int numberOfRides, double totalFare, double averageFare)
+        {
+            this.numberOfRides = numberOfRides;
+            this.totalFare = totalFare;
+            this.averageFare = averageFare;
+        }
+    }
+}
