a229ed4805d92ac60c1a4d5a3d5b86fc0e05ecb3
Satantra Tewari <satantra1508@gmail.com>
2020-11-05 19:59:25 +0530

    [satantra] add. enhanced invoice summary generated

--- Cab_Invoice_Generator/Invoice_Summary.cs
+++ Cab_Invoice_Generator/Invoice_Summary.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace Cab_Invoice_Generator
 {
-    class Invoice_Summary
+    public class Invoice_Summary
     {
+        Invoice_Data invoiceData;
+        public Invoice_Data GetInvoice(int numberOfRides, double totalFare)
+        {
+            double averageFare = totalFare / numberOfRides;
+            invoiceData = new Invoice_Data(numberOfRides, totalFare, averageFare);
+            return invoiceData;
+        }
     }
 }
