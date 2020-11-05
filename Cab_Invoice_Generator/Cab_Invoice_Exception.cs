b544aa3f5a52de00e7d82e2285269d74028cd86f
Satantra Tewari <satantra1508@gmail.com>
2020-11-05 19:19:23 +0530

    [satantra] add. exception class storing different types of exceptions

---
+++ Cab_Invoice_Generator/Cab_Invoice_Exception.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cab_Invoice_Generator
+{
+    public class Cab_Invoice_Exception:Exception
+    {
+        public enum ExceptionType
+        {
+            //storing different types of exceptions that can occur
+            INVALID_RIDE_TYPE,
+            INVALID_DISTANCE,
+            INVALID_TIME,
+            INVALID_USERID,
+            NULL_RIDES
+        }
+        public ExceptionType exceptionType;
+        public Cab_Invoice_Exception(ExceptionType exceptionType, string message):base(message)
+        {
+            //parameterized constructor
+            this.exceptionType = exceptionType;
+        }
+    }
+}
